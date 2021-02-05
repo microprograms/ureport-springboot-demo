f5d535ef4c764fb17f62d78780bcd60d05a73d94
xuzewei <microprograms@126.com>
2021-02-05 17:52:17 +0800

    EsDataLoader.java

--- src/main/java/com/github/microprograms/ureport_springboot_demo/EsDataLoader.java
+++ src/main/java/com/github/microprograms/ureport_springboot_demo/EsDataLoader.java
@@ -3,7 +3,6 @@ package com.github.microprograms.ureport_springboot_demo;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,20 +13,15 @@ import com.alibaba.fastjson.JSONObject;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.DeprecationHandler;
-import org.elasticsearch.common.xcontent.NamedXContentRegistry;
-import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.common.xcontent.XContentParser;
-import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.search.SearchModule;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.HttpClientUtils;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 
 public class EsDataLoader {
 
@@ -47,29 +41,29 @@ public class EsDataLoader {
         return config.getJSONObject("datasources").getJSONObject(dsName);
     }
 
-    public List<Map<String, String>> loadReportData(String dsName, String datasetName, Map<String, Object> parameters)
+    public List<?> loadReportData(String dsName, String datasetName, Map<String, Object> parameters)
             throws IOException {
         JSONObject dsConfig = getDsConfig(dsName);
-        String[] indices = dsConfig.getJSONArray("indices").toArray(new String[] {});
+        String url = dsConfig.getString("url");
         String source = dsConfig.getJSONObject("search").toJSONString();
         String path = dsConfig.getJSONObject("datasets").getJSONObject(datasetName).getString("path");
 
-        HttpHost httpHost = new HttpHost("127.0.0.1", 9200, "http");
-        String response = search(httpHost, source, indices).toString();
+        String response = search(url, source);
         JSONObject jsonObject = JSON.parseObject(response);
         JSONArray jsonArray = getJsonArray(jsonObject, path);
+        return jsonArray;
 
-        List<Map<String, String>> list = new ArrayList<>();
-        for (int i = 0; i < jsonArray.size(); i++) {
-            JSONObject x = jsonArray.getJSONObject(i);
-            Map<String, String> map = new HashMap<>();
-            for (String key : x.keySet()) {
-                map.put(key, x.getString(key));
-            }
-            list.add(map);
-        }
+        // List<Map<String, String>> list = new ArrayList<>();
+        // for (int i = 0; i < jsonArray.size(); i++) {
+        // JSONObject x = jsonArray.getJSONObject(i);
+        // Map<String, String> map = new HashMap<>();
+        // for (String key : x.keySet()) {
+        // map.put(key, x.getString(key));
+        // }
+        // list.add(map);
+        // }
 
-        return list;
+        // return list;
     }
 
     public static JSONArray getJsonArray(JSONObject jsonObject, String path) {
@@ -80,15 +74,17 @@ public class EsDataLoader {
         return getJsonArray(jsonObject.getJSONObject(path.substring(0, index)), path.substring(index + 1));
     }
 
-    public static SearchResponse search(HttpHost httpHost, String source, String... indices) throws IOException {
-        SearchModule searchModule = new SearchModule(Settings.EMPTY, false, Collections.emptyList());
-        XContentParser parser = XContentFactory.xContent(XContentType.JSON).createParser(
-                new NamedXContentRegistry(searchModule.getNamedXContents()),
-                DeprecationHandler.THROW_UNSUPPORTED_OPERATION, source);
-        SearchRequest searchRequest = new SearchRequest(indices);
-        searchRequest.source(SearchSourceBuilder.fromXContent(parser));
-        try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(httpHost))) {
-            return client.search(searchRequest, RequestOptions.DEFAULT);
+    public static String search(String url, String source) throws IOException {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpPost request = new HttpPost(url);
+            request.addHeader("Content-Type", "application/json");
+            request.setEntity(new StringEntity(source, "utf8"));
+            HttpResponse httpResponse = httpClient.execute(request);
+            int statusCode = httpResponse.getStatusLine().getStatusCode();
+            if (statusCode != HttpStatus.SC_OK) {
+                throw new IOException("statusCode=" + statusCode);
+            }
+            return EntityUtils.toString(httpResponse.getEntity(), "utf8");
         }
     }
 }
